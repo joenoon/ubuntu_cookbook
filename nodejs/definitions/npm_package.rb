4b34199c82338e7020282a669e080bd724e15cb3
Joe Noon <joenoon@gmail.com>
2010-08-27 13:14:55 -0700

    npm_package def

---
+++ nodejs/definitions/npm_package.rb
@@ -0,0 +1,11 @@
+define :npm_package, :args => nil, :libdir => nil do
+  include_recipe "node"
+  _name = params[:name]
+  _args = params[:args] || _name
+  _libdir = params[:libdir] || "/usr/local/lib/node"
+  execute "install npm package #{_args}" do
+    command "npm install #{_args}"
+    user "root"
+    not_if "test -e #{_libdir}/#{_name}*"
+  end
+end
