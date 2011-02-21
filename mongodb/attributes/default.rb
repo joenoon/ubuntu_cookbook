1ae4e7409f42b466e117dbf071b18d92084be0f4
Joe Noon <joenoon@gmail.com>
2011-02-21 03:55:34 -0800

    mongodb mod

--- mongodb/attributes/default.rb
+++ mongodb/attributes/default.rb
@@ -1,4 +1,1 @@
-arch = node[:kernel][:machine] == "x86_64" ? "x86_64" : "i686"
-
-set_unless[:mongodb][:arch] = arch
-set_unless[:mongodb][:version] = "1.6.1"
+set_unless[:mongodb][:ubuntu_version] = '10.4'
--- mongodb/recipes/default.rb
+++ mongodb/recipes/default.rb
@@ -1,53 +1,16 @@
-user "mongodb" do
-  shell "/bin/bash"
-end
-
-cookbook_file "/etc/init/mongodb.conf" do
-  source "upstart.conf"
-  owner "root"
-  group "root"
-  mode "0644"
-end
-
-cookbook_file "/etc/mongodb.conf" do
-  source "mongodb.conf"
-  owner "root"
-  group "root"
-  mode "0644"
-end
-
-dl_mongo_dir = "mongodb-linux-#{node[:mongodb][:arch]}-#{node[:mongodb][:version]}"
-tgz_mongo = "#{dl_mongo_dir}.tgz"
-usr_src_file = "/usr/local/src/#{tgz_mongo}"
-url_mongo = "http://fastdl.mongodb.org/linux/#{tgz_mongo}"
-
-bash "install mongodb" do
-  user "root"
-  cwd "/usr/local/src"
-  code <<-CODE
-    rm -rf #{dl_mongo_dir} /usr/mongodb
-    tar zxf #{tgz_mongo}
-    mv #{dl_mongo_dir} /usr/mongodb
-    ln -nfs /usr/mongodb/bin/mongod /usr/bin/mongod
-  CODE
+execute "touch /etc/apt/mongo_gpg_key_added" do
   action :nothing
 end
 
-remote_file usr_src_file do
-  source url_mongo
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create_if_missing
-  notifies :run, resources(:bash => "install mongodb"), :immediately
+execute "add mongo gpg key" do
+  command "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
+  not_if "test -e /etc/apt/mongo_gpg_key_added"
+  notifies :run, resources(:execute => "touch /etc/apt/mongo_gpg_key_added"), :immediately
+  notifies :run, resources(:execute => "apt-get update"), :immediately
 end
 
-%w( /var/lib/mongodb /var/log/mongodb ).each do |d|
-  directory d do
-    owner "mongodb"
-    group "mongodb"
-    mode "0755"
-  end
+template "/etc/apt/sources.list.d/mongodb.list" do
+  source "mongodb.list.erb"
 end
 
 service "mongodb" do
