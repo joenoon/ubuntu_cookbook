67cc97c82e8d3e8b253dbfc6812ff34656c647d1
Joe Noon <joenoon@gmail.com>
2011-03-06 00:39:35 -0800

    opts

--- rvm/definitions/rvm_gem_package.rb
+++ rvm/definitions/rvm_gem_package.rb
@@ -6,5 +6,6 @@ define :rvm_gem_package, :ruby_wrapper => nil, :source => nil, :version => nil d
     source opts[:source] if opts[:source]
     version opts[:version] if opts[:version]
     gem_binary "rvm #{rb} gem"
+    options "--no-ri --no-rdoc"
   end
 end
