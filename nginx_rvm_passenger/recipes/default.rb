7a0f4c5caa1db6ad3b94ea6c867de9767fefc753
Joe Noon <joenoon@gmail.com>
2011-03-05 23:13:44 -0800

    update

--- nginx_rvm_passenger/recipes/default.rb
+++ nginx_rvm_passenger/recipes/default.rb
@@ -1,28 +1,30 @@
-require 'digest/md5'
-md5_dir = "passenger-"+Digest::MD5.hexdigest(node[:nginx_rvm_passenger][:passenger_tgz]).to_s
-
 rvm_install node[:nginx_rvm_passenger][:ruby_wrapper]
+
+rvm_gem_package "passenger" do
+  action :install
+  if node[:nginx_rvm_passenger][:passenger_version]
+    version node[:nginx_rvm_passenger][:passenger_version]
+  end
+  ruby_wrapper node[:nginx_rvm_passenger][:ruby_wrapper]
+end
 
 bash "install passenger" do
   code %Q{
     . /usr/local/lib/rvm
     rvm use #{node[:nginx_rvm_passenger][:ruby_wrapper]}
-    mkdir -p #{md5_dir}
-    cd #{md5_dir}
-    curl -L #{node[:nginx_rvm_passenger][:passenger_tgz]} | tar xzf - --strip-components=1
-    bin/passenger-install-nginx-module --auto --auto-download --prefix=#{node[:nginx_rvm_passenger][:prefix]} --extra-configure-flags='--with-http_ssl_module'
-    touch chef_complete
+    passenger-install-nginx-module --auto --auto-download --prefix=#{node[:nginx_rvm_passenger][:prefix]} --extra-configure-flags='--with-http_ssl_module'
   }
-  cwd "/usr/local/src"
-  not_if "test -e /usr/local/src/#{md5_dir}/chef_complete"
+  create "#{node[:nginx_rvm_passenger][:prefix]}/sbin/nginx"
 end
 
 directory "#{node[:nginx_rvm_passenger][:prefix]}/conf/conf.d" do
   mode "0755"
+  recursive true
 end
 
 directory "#{node[:nginx_rvm_passenger][:prefix]}/conf/sites" do
   mode "0755"
+  recursive true
 end
 
 template "/etc/init.d/nginx" do
@@ -39,8 +41,8 @@ template "#{node[:nginx_rvm_passenger][:prefix]}/conf/conf.d/passenger.conf" do
   source "passenger.conf"
   mode "0644"
   notifies :restart, resources(:service => "nginx")
-  variables :passenger_root => "/usr/local/src/#{md5_dir}".to_s.strip,
-            :passenger_ruby_home => `bash -l -c "echo $MY_RUBY_HOME"`.to_s.strip
+  variables :passenger_root => `bash -l -c ". /usr/local/lib/rvm; passenger-config --root`.to_s.strip,
+            :passenger_ruby_home => `bash -l -c ". /usr/local/lib/rvm; echo $MY_RUBY_HOME"`.to_s.strip
 end
 
 template "#{node[:nginx_rvm_passenger][:prefix]}/conf/nginx.conf" do
--- rvm/definitions/rvm_install.rb
+++ rvm/definitions/rvm_install.rb
@@ -4,7 +4,7 @@ define :rvm_install do
   bash "rvm install #{rb}" do
     code %Q{
       rvm install #{rb}
-      rvm #{rb} gem install rake bundler
+      rvm #{rb} gem install rake bundler rack
     }
     only_if "rvm use #{rb} | grep not | grep installed"
   end
