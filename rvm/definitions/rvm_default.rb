89f5ca499ef5b728011c18e0e1cb8d6bf2a12866
Joe Noon <joenoon@gmail.com>
2011-03-06 00:17:20 -0800

    loop

--- rvm/definitions/rvm_default.rb
+++ rvm/definitions/rvm_default.rb
@@ -1,6 +1,5 @@
 define :rvm_default do
   rb = params[:name]
-  rvm_install rb
   bash "rvm #{rb} --default" do
     code "rvm #{rb} --default"
     only_if "rvm list strings | grep #{rb}"
--- rvm/definitions/rvm_gem_package.rb
+++ rvm/definitions/rvm_gem_package.rb
@@ -2,7 +2,6 @@
 define :rvm_gem_package, :ruby_wrapper => nil, :source => nil, :version => nil do
   opts = params
   rb = opts[:ruby_wrapper]
-  rvm_install rb
   gem_package opts[:name] do
     source opts[:source] if opts[:source]
     version opts[:version] if opts[:version]
